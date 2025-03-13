2d8fe58665ed7b8e53da196637cd6d7c04a01c56
d3Cod3Monk <50435516+d3cod3monk78@users.noreply.github.com>
2025-03-13 23:23:10 +0530

    add two files flatmate and prd_report

---
+++ flatmates-bill/flatmate.py
@@ -0,0 +1,10 @@
+from bill import Bill
+
+
+class Flatmate:
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        pass
---
+++ flatmates-bill/pdf_report.py
@@ -0,0 +1,11 @@
+from flatmate import Flatmate
+from bill import Bill
+
+
+class PDFReport:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate_one, flatmate_two, bill):
+        pass
+    
